89456c8564b5edf3144e8242a71adeaf15990899
NShio0114 <s2332116qw@s.chibakoudai.jp>
2025-10-10 16:45:11 +0900

    パズルボードの作成

--- gameSceneView.php
+++ gameSceneView.php
@@ -1,10 +1,7 @@
 <div id="game-screen" class="scene-view screen active">
     <div id="side-panel">
-        <div class="score-display">
-            <h2>スコア</h2>
-            <p id="score"><?php echo $gameModel->score; ?></p>
+        <div id="puzzle-board">
         </div>
-        <hr>
         <form method="POST" action="index.php">
             <input type="hidden" name="action" value="titleScene">
             <button id="back-to-title" class="control-button" type="submit">タイトルへ戻る</button>
--- titleSceneView.php
+++ titleSceneView.php
@@ -1,7 +1,7 @@
 <div id="title-screen" class="scene-view screen active">
-    <h1>3 MATCH PUZZLE</h1>
-    <form method="POST" action="index.php">
-        <input type="hidden" name="action" value="gameScene">
-        <button id="start-button" type="submit">ゲームスタート</button>
-    </form>
+    <h1>3 MATCH PUZZLE</h1>
+    <form method="POST" action="index.php">
+        <input type="hidden" name="action" value="gameScene">
+        <button id="start-button" type="submit">ゲームスタート</button>
+        </form>
 </div>
