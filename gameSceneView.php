53f021efbccc6a8b44c5342c35e05c2271a00701
NShio0114 <s2332116qw@s.chibakoudai.jp>
2025-10-10 17:00:37 +0900

    タイトルへ戻るボタンの追加

--- gameSceneView.php
+++ gameSceneView.php
@@ -1,10 +1,11 @@
-<div id="game-screen" class="scene-view screen active">
-    <div id="side-panel">
+<div id="game-container">
+    <form method="POST" action="index.php" id="title-return-form">
+        <input type="hidden" name="action" value="titleScene">
+        <button id="to-title-button" class="control-button" type="submit">タイトルへ戻る</button>
+    </form>
+
+    <div id="game-screen" class="scene-view screen active">
         <div id="puzzle-board">
         </div>
-        <form method="POST" action="index.php">
-            <input type="hidden" name="action" value="titleScene">
-            <button id="back-to-title" class="control-button" type="submit">タイトルへ戻る</button>
-        </form>
-    </div>
+        </div>
 </div>
