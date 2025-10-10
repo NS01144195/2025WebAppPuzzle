e3dd810ffefc91d337e04b80e90797d8ae4d00d2
NShio0114 <s2332116qw@s.chibakoudai.jp>
2025-10-10 17:07:07 +0900

    ボードにマス目を追加

--- gameSceneView.php
+++ gameSceneView.php
@@ -6,6 +6,13 @@
 
     <div id="game-screen" class="scene-view screen active">
         <div id="puzzle-board">
+            <?php
+            for ($row = 0; $row < 9; $row++) {
+                for ($col = 0; $col < 9; $col++) {
+                    echo '<div class="cell" data-row="' . $row . '" data-col="' . $col . '"></div>';
+                }
+            }
+            ?>
         </div>
-        </div>
+    </div>
 </div>
