dabbf607529285354ca89a5776be908ac850c487
NShio0114 <s2332116qw@s.chibakoudai.jp>
2025-10-10 15:50:00 +0900

    タイトルシーンの作成

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1280, height=720">
+    <title>3 Match Puzzle</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="game-container">
+        <div id="title-screen">
+            <h1>3 MATCH PUZZLE</h1>  
+            <button id="start-button">ゲームスタート</button>
+            <div class="footer-note">
+                2332116 Nakamura Shio
+            </div>
+        </div>
+        </div>
+</body>
