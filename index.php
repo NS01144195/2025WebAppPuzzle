e52d3502fb8bb72fa863de54d2879147f0a53b21
salt01144195 <131969133+salt01144195@users.noreply.github.com>
2025-10-12 18:32:12 +0900

    修正

--- index.php
+++ index.php
@@ -5,19 +5,6 @@ require_once 'PuzzleManager.php';
 // PuzzleManager をセッションから取得
 if (!isset($_SESSION['puzzleManager'])) {
     $puzzleManager = new PuzzleManager();
-    $puzzleManager->startGame();
-    $testBoard = [
-    [1,2,3,4,5,1,2,3,4],
-    [2,3,2,5,1,2,3,4,5],
-    [3,4,5,1,2,3,4,5,1],
-    [4,5,1,2,3,4,5,1,2],
-    [5,1,2,3,4,5,1,2,3],
-    [1,2,3,4,5,1,2,3,4],
-    [2,3,4,5,1,2,3,4,5],
-    [3,4,5,1,2,3,4,5,1],
-    [4,5,1,2,3,4,5,1,2],
-];
-$puzzleManager->setBoard($testBoard);
     $_SESSION['puzzleManager'] = serialize($puzzleManager);
 } else {
     $puzzleManager = unserialize($_SESSION['puzzleManager']);
@@ -45,38 +32,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
 
 // ===== 画面遷移処理 =====
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
-    if ($_POST['action'] === 'gameScene') {
-        $_SESSION['current_scene'] = 'game';
+    switch($_POST['action']) {
+        case 'gameScene':
+            $_SESSION['current_scene'] = 'game';
 
-        // ゲーム開始時にPuzzleManagerを初期化
-        $puzzleManager = new PuzzleManager();
-        $puzzleManager->startGame();
+            // 毎回PuzzleManagerを初期化
+            $puzzleManager = new PuzzleManager();
+            $puzzleManager->startGame();
+/*
+            // テスト盤面をセット
+            $testBoard = [
+                [1,2,3,4,5,1,2,3,4],
+                [2,3,2,5,1,2,3,4,5],
+                [3,4,5,1,2,3,4,5,1],
+                [4,5,1,2,3,4,5,1,2],
+                [5,1,2,3,4,5,1,2,3],
+                [1,2,3,4,5,1,2,3,4],
+                [2,3,4,5,1,2,3,4,5],
+                [3,4,5,1,2,3,4,5,1],
+                [4,5,1,2,3,4,5,1,2],
+            ];
+            $puzzleManager->setBoard($testBoard);
+*/
+            $_SESSION['puzzleManager'] = serialize($puzzleManager);
+            break;
 
-        // 盤面をテストボードで上書きしたい場合はここでセット
-        $testBoard = [
-            [1,2,3,4,5,1,2,3,4],
-            [2,3,2,5,1,2,3,4,5],
-            [3,4,5,1,2,3,4,5,1],
-            [4,5,1,2,3,4,5,1,2],
-            [5,1,2,3,4,5,1,2,3],
-            [1,2,3,4,5,1,2,3,4],
-            [2,3,4,5,1,2,3,4,5],
-            [3,4,5,1,2,3,4,5,1],
-            [4,5,1,2,3,4,5,1,2],
-        ];
-        $puzzleManager->setBoard($testBoard);
-
-        // セッションに保存
-        $_SESSION['puzzleManager'] = serialize($puzzleManager);
-        $_SESSION['board'] = $puzzleManager->getBoard();
-    } elseif ($_POST['action'] === 'titleScene') {
-        $_SESSION['current_scene'] = 'title';
+        case 'titleScene':
+            $_SESSION['current_scene'] = 'title';
+            unset($_SESSION['puzzleManager']); // もしゲームデータを破棄したい場合
+            break;
     }
 
-    // リダイレクトして二重送信防止
+    // 二重送信防止
     header('Location: index.php');
     exit;
 }
+
 
 // 現在のシーン
 if (!isset($_SESSION['current_scene'])) {
